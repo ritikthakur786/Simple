63b3cab8ad81779bfa5303dfdf292373a1d7ee50
ritikthakur786 <iamrkay786@gmail.com>
2025-04-17 15:41:00 +0530

    Update buttons.py

--- SONALI_MUSIC/help/buttons.py
+++ SONALI_MUSIC/help/buttons.py
@@ -84,15 +84,15 @@ class BUTTONS(object):
     
     SBUTTON = [
         [
-            InlineKeyboardButton("ϻυsɪᴄ", callback_data="settings_back_helper"),
-            InlineKeyboardButton("ϻᴧηᴧɢєϻєηᴛ", callback_data="MANAGEMENT_CP"),
+            InlineKeyboardButton("ʙσт нєʟᴘ", callback_data="settings_back_helper"),
+            InlineKeyboardButton("ᴧʟʟ ʙσᴛ's", callback_data="ALLBOT_CP"),
         ],
         [
-            InlineKeyboardButton("ᴛσσʟs", callback_data="TOOL_CP"),
+            InlineKeyboardButton("ᴘʀσϻσᴛɪση", callback_data="PROMOTION_CP"),
             InlineKeyboardButton("ᴧʙσυт", callback_data="MAIN_BACK HELP_ABOUT"),
         ],
         [
             InlineKeyboardButton("⌯ ʙᴧᴄᴋ ᴛσ ʜσϻє ⌯", callback_data="settingsback_helper"),
-            
+
         ]
     ]
